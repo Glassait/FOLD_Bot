d6015300d98565932306cd0a69dedda1da837fa2
Tendzin ROFFLER <40204866+Glassait@users.noreply.github.com>
2023-11-15 19:29:04 +0100

    Extend auto reply + new status (#3)

--- src/events/message-update.event.ts
+++ src/events/message-update.event.ts
@@ -3,9 +3,9 @@ import { BotEvent } from '../types/bot-event.type';
 import { AutoReplyUtils } from '../utils/auto-reply.utils';
 
 const event: BotEvent = {
-    name: Events.MessageCreate,
+    name: Events.MessageUpdate,
     once: false,
-    async execute(_client: Client, message: Message): Promise<void> {
+    async execute(_client: Client, _oldMessage: Message, message: Message): Promise<void> {
         await AutoReplyUtils.autoReply(message);
     },
 };
--- src/events/ready.event.ts
+++ src/events/ready.event.ts
@@ -1,16 +1,19 @@
-import { ActivityType, Client, Events } from 'discord.js';
+import { Client, Events } from 'discord.js';
 import { BotEvent } from '../types/bot-event.type';
+import { SentenceUtils } from '../utils/sentence.utils';
 
 const event: BotEvent = {
     name: Events.ClientReady,
     once: true,
     async execute(client: Client): Promise<void> {
         console.log(`💪 Logged in as ${client.user?.tag}`);
+        const status = SentenceUtils.getRandomStatus();
+
         client.user?.setPresence({
             activities: [
                 {
-                    type: ActivityType.Playing,
-                    name: 'Imagine un monde sans Bady',
+                    type: status[0],
+                    name: status[1],
                 },
             ],
             status: 'online',
