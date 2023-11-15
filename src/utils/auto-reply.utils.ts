350aec82162c66ed2514a2d452f3eda34de55e63
Glassait <tendzinroffler@gmail.com>
2023-11-15 19:26:48 +0100

    Extend auto reply + new status

---
+++ src/utils/auto-reply.utils.ts
@@ -0,0 +1,34 @@
+import { Collection, Message, User } from 'discord.js';
+import { FeatureSingleton } from '../singleton/feature.singleton';
+import { DiscordId, Reply } from '../types/feature.type';
+import { SentenceUtils } from './sentence.utils';
+
+export class AutoReplyUtils {
+    public static async autoReply(message: Message): Promise<void> {
+        if (message.author.bot) {
+            return;
+        }
+
+        const mention: Collection<string, User> = message.mentions.users;
+
+        if (mention.size === 0) {
+            return;
+        }
+
+        const feature: FeatureSingleton = FeatureSingleton.instance;
+        const id: DiscordId = message.author.id;
+        const autoReply: Reply[] = feature.getArrayFromReplyto(id);
+
+        if (!autoReply) {
+            return;
+        }
+
+        let hasAutoReply: boolean = mention.some((user: User): boolean =>
+            autoReply.some((reply: Reply): boolean => reply.activateFor === user.id)
+        );
+
+        if (hasAutoReply) {
+            await message.channel.send(SentenceUtils.getRandomResponse(id));
+        }
+    }
+}
