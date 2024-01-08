3f5f3c4089a1d5100f11d87a0528db4a2c0b7d3e
glassait <tendzinroffler@gmail.com>
2024-01-08 17:25:27 +0100

    patch: rework axios

--- src/module/feature/loops/fold-recrutement.loop.ts
+++ src/module/feature/loops/fold-recrutement.loop.ts
@@ -4,6 +4,8 @@ import { Context } from '../../shared/classes/context';
 import { FeatureSingleton } from '../../shared/singleton/feature.singleton';
 import { FoldRecrutementModel } from './model/fold-recrutement.model';
 import { EmojiEnum } from '../../shared/enums/emoji.enum';
+import { EnvUtil } from '../../shared/utils/env.util';
+import { TimeEnum } from '../../shared/enums/time.enum';
 
 const logger: Logger = new Logger(new Context('Fold-Recrutement-LOOP'));
 
@@ -11,13 +13,19 @@ module.exports = async (client: Client): Promise<void> => {
     const feature: FeatureSingleton = FeatureSingleton.instance;
     const recrutement: FoldRecrutementModel = new FoldRecrutementModel();
     await recrutement.fetchMandatory(client);
+    let numberOfClans: number = 0;
 
     logger.info(`${EmojiEnum.LOOP} Start fold-Recrutement loop`);
     for (const clan of feature.clans) {
         logger.info(`${EmojiEnum.LOOP} Start fold-Recrutement loop for ${clan.name}`);
-        await recrutement.fetchClanActivity(clan.id);
-        await recrutement.sendMessageToChannelFromExtractedPlayer(clan);
+        await recrutement.fetchClanActivity(clan);
         logger.info(`${EmojiEnum.LOOP} End fold-Recrutement loop for ${clan.name}`);
+
+        numberOfClans++;
+        if (numberOfClans >= 30) {
+            await EnvUtil.sleep(TimeEnum.SECONDE * 30);
+            numberOfClans = 0;
+        }
     }
     await recrutement.sendFooter();
     logger.info(`${EmojiEnum.LOOP} End fold-Recrutement loop`);
