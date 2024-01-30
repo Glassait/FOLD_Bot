a13273f67c4416d3d3f00d4f009e05ad574e601b
Tendzin ROFFLER <40204866+Glassait@users.noreply.github.com>
2024-01-30 22:45:29 +0100

    release: fix sentence and add check for month stats (#94)

--- src/module/feature/events/ready.event.ts
+++ src/module/feature/events/ready.event.ts
@@ -46,41 +46,65 @@ export const event: BotEvent = {
                 (a: [string, TriviaPlayerStatisticType], b: [string, TriviaPlayerStatisticType]) => b[1][monthStr].elo - a[1][monthStr].elo
             );
 
+            const listEmber: EmbedBuilder[] = [];
+
             const embedInt = new EmbedBuilder()
                 .setTitle(`Résumé du mois de ${monthStr}`)
                 .setDescription('Le dernier mois a été chargé en apprentissage. Découvrons les statistiques du mois.')
                 .setImage('https://us-wotp.wgcdn.co/dcont/fb/image/wall_february_2018_1024x600.jpg')
                 .setColor(Colors.DarkGold);
 
+            listEmber.push(embedInt);
+
             let index = 0;
-            const embedScoreboard = new EmbedBuilder()
-                .setTitle('Tableau des scores')
-                .setDescription(
-                    'Nous allons visualiser dans un premier temps le score des joueurs. \n(Plus de détails avec la commande `/trivia-statistics)`'
-                )
-                .setColor(Colors.DarkGold)
-                .setFields(
-                    {
-                        name: 'Leaderboard',
-                        value:
-                            'Les trois meilleurs joueurs du mois sont : \n\n' +
-                            playerClassement.slice(0, 3).reduce((text: string, player: [string, TriviaPlayerStatisticType]) => {
-                                text += `${MEDAL[index]} ${player[0]} avec \`${player[1][monthStr].elo}\` points\n`;
-                                index++;
-                                return text;
-                            }, ''),
-                    },
-                    {
+
+            if (playerClassement?.length > 0) {
+                const embedScoreboard = new EmbedBuilder()
+                    .setTitle('Tableau des scores')
+                    .setDescription(
+                        'Nous allons visualiser dans un premier temps le score des joueurs. \n(Plus de détails avec la commande `/trivia-statistics)`'
+                    )
+                    .setColor(Colors.DarkGold)
+                    .setFields(
+                        {
+                            name: 'Leaderboard',
+                            value:
+                                'Les trois meilleurs joueurs du mois sont : \n\n' +
+                                playerClassement.slice(0, 3).reduce((text: string, player: [string, TriviaPlayerStatisticType]) => {
+                                    text += `${MEDAL[index]} ${player[0]} avec \`${player[1][monthStr].elo}\` points\n`;
+                                    index++;
+                                    return text;
+                                }, ''),
+                        },
+                        {
+                            name: 'Scoreboard',
+                            value:
+                                'Voila le reste du classement :\n\n' +
+                                playerClassement.slice(3, -1).reduce((text: string, player: [string, TriviaPlayerStatisticType]) => {
+                                    text += `${index + 1} : ${player[0]} avec \`${player[1][monthStr].elo}\` points\n`;
+                                    index++;
+                                    return text;
+                                }, ''),
+                        }
+                    );
+
+                listEmber.push(embedScoreboard);
+            } else {
+                const embedNobody = new EmbedBuilder()
+                    .setTitle('Tableau des scores')
+                    .setDescription(
+                        'Nous allons visualiser dans un premier temps le score des joueurs. \n(Plus de détails avec la commande `/trivia-statistics)`'
+                    )
+                    .setColor(Colors.DarkGold)
+                    .setFields({
                         name: 'Scoreboard',
-                        value:
-                            'Voila le reste du classement :\n\n' +
-                            playerClassement.slice(3, -1).reduce((text: string, player: [string, TriviaPlayerStatisticType]) => {
-                                text += `${index + 1} : ${player[0]} avec \`${player[1][monthStr].elo}\` points\n`;
-                                index++;
-                                return text;
-                            }, ''),
-                    }
-                );
+                        value: "Il semblerait qu'un joueur n'ai participé au trivia game durant ce mois (っ °Д °;)っ",
+                    })
+                    .setImage(
+                        'https://media.istockphoto.com/id/1124667817/photo/desperate-upset-teen-girl-victim-crying-alone-at-home.jpg?s=612x612&w=0&k=20&c=GBExtQT11X25SZxVrqCNs8RURDVz-yVELpJqoFogy9w='
+                    );
+                listEmber.push(embedNobody);
+            }
 
             playerClassement.sort(
                 (a: [string, TriviaPlayerStatisticType], b: [string, TriviaPlayerStatisticType]) =>
@@ -88,53 +112,71 @@ export const event: BotEvent = {
             );
 
             const quickPlayer: [string, TriviaPlayerStatisticType] = playerClassement[0];
-            const embedQuick = new EmbedBuilder()
-                .setTitle('Le joueur le plus rapide')
-                .setDescription(
-                    `Tel un EBR 75, \`${quickPlayer[0]}\` détruit ces ennemies plus vite que l'éclair.` +
-                        `Ainsi il a répondu le plus rapidement en \`${
-                            Math.min(...quickPlayer[1][monthStr].answer_time) / TimeEnum.SECONDE
-                        }\` secondes.`
-                )
-                .setColor(Colors.DarkGold)
-                .setImage('https://images6.alphacoders.com/131/1315267.jpeg');
+
+            if (quickPlayer?.length > 0) {
+                const embedQuick = new EmbedBuilder()
+                    .setTitle('Le joueur le plus rapide')
+                    .setDescription(
+                        `Tel un EBR 75, \`${quickPlayer[0]}\` détruit ces ennemies plus vite que l'éclair.` +
+                            `Ainsi il a répondu le plus rapidement en \`${
+                                Math.min(...quickPlayer[1][monthStr].answer_time) / TimeEnum.SECONDE
+                            }\` secondes.`
+                    )
+                    .setColor(Colors.DarkGold)
+                    .setImage('https://images6.alphacoders.com/131/1315267.jpeg');
+
+                listEmber.push(embedQuick);
+            }
 
             const slowPlayer: [string, TriviaPlayerStatisticType] = playerClassement[playerClassement.length - 1];
-            const embedSlow = new EmbedBuilder()
-                .setTitle('Le joueur le plus lent')
-                .setDescription(
-                    `\`${slowPlayer[0]}\` est un véritable mur d'acier IRL, du coup il prend son temps pour répondre.` +
-                        `Son temps le plus long est de \`${Math.min(...slowPlayer[1][monthStr].answer_time) / 1000}\` secondes.`
-                )
-                .setColor(Colors.DarkGold)
-                .setImage('https://static-ptl-eu.gcdn.co/dcont/fb/image/wot_wallpaperseptember2015_eng_1024x600_eng.jpg');
+
+            if (slowPlayer?.length > 0) {
+                const embedSlow = new EmbedBuilder()
+                    .setTitle('Le joueur le plus lent')
+                    .setDescription(
+                        `\`${slowPlayer[0]}\` est un véritable mur d'acier IRL, du coup il prend son temps pour répondre.` +
+                            ` Son temps le plus long est de \`${Math.min(...slowPlayer[1][monthStr].answer_time) / 1000}\` secondes.`
+                    )
+                    .setColor(Colors.DarkGold)
+                    .setImage('https://static-ptl-eu.gcdn.co/dcont/fb/image/wot_wallpaperseptember2015_eng_1024x600_eng.jpg');
+                listEmber.push(embedSlow);
+            }
 
             playerClassement.sort(
                 (a: [string, TriviaPlayerStatisticType], b: [string, TriviaPlayerStatisticType]) =>
                     b[1][monthStr].win_strick.max - a[1][monthStr].win_strick.max
             );
             const winStrickPlayer: [string, TriviaPlayerStatisticType] = playerClassement[0];
-            const embedWinStrick = new EmbedBuilder()
-                .setTitle("Le joueur avec le plus de bonnes réponses d'affilée")
-                .setDescription(
-                    `\`${winStrickPlayer[0]}\` est tel un Léopard, il rate jamais ça cible. \`${winStrickPlayer[0]}\` a correctement répondu \`${winStrickPlayer[1][monthStr].win_strick.max}\` fois d'affilée. `
-                )
-                .setColor(Colors.DarkGold)
-                .setImage(
-                    'https://worldoftanks.eu/dcont/fb/media/july_2013_wallpaper/normal/july2013_1920x1080_cl.jpg?MEDIA_PREFIX=/dcont/fb/'
-                );
 
-            const embedOverall = new EmbedBuilder()
-                .setTitle('Statistique générale')
-                .setDescription('Pour finir voici des statistiques inutiles.')
-                .setColor(Colors.DarkGold)
-                .setFields(
-                    { name: 'Nombre total de parties :', value: `\`${stats.overall[monthStr].number_of_game}\`` },
-                    {
-                        name: 'Nombre de parties sans participation :',
-                        value: `\`${stats.overall[monthStr].game_without_participation}\``,
-                    }
-                );
+            if (winStrickPlayer?.length > 0) {
+                const embedWinStrick = new EmbedBuilder()
+                    .setTitle("Le joueur avec le plus de bonnes réponses d'affilée")
+                    .setDescription(
+                        `\`${winStrickPlayer[0]}\` est tel un Léopard, il rate jamais ça cible. \`${winStrickPlayer[0]}\` a correctement répondu \`${winStrickPlayer[1][monthStr].win_strick.max}\` fois d'affilée. `
+                    )
+                    .setColor(Colors.DarkGold)
+                    .setImage(
+                        'https://worldoftanks.eu/dcont/fb/media/july_2013_wallpaper/normal/july2013_1920x1080_cl.jpg?MEDIA_PREFIX=/dcont/fb/'
+                    );
+
+                listEmber.push(embedWinStrick);
+            }
+
+            if (stats.overall[monthStr]) {
+                const embedOverall = new EmbedBuilder()
+                    .setTitle('Statistique générale')
+                    .setDescription('Pour finir voici des statistiques inutiles.')
+                    .setColor(Colors.DarkGold)
+                    .setFields(
+                        { name: 'Nombre total de parties :', value: `\`${stats.overall[monthStr].number_of_game}\`` },
+                        {
+                            name: 'Nombre de parties sans participation :',
+                            value: `\`${stats.overall[monthStr].game_without_participation}\``,
+                        }
+                    );
+
+                listEmber.push(embedOverall);
+            }
 
             const embedFeedback = new EmbedBuilder()
                 .setTitle('Feedback')
@@ -143,10 +185,12 @@ export const event: BotEvent = {
                     "Merci d'avoir participé tout au long de ce mois. Si vous avez des feedbacks à me faire (positif ou négatif) je suis preneur (❤️ω❤️)"
                 );
 
+            listEmber.push(embedFeedback);
+
             logger.info('Sending trivia month message...');
             await channel.send({
                 content: '@here',
-                embeds: [embedInt, embedScoreboard, embedQuick, embedSlow, embedWinStrick, embedOverall, embedFeedback],
+                embeds: listEmber,
             });
         }
     },
