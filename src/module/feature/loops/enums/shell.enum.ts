d887c111c7f75fa08277edba2eb0107371aa67b3
glassait <tendzinroffler@gmail.com>
2024-02-06 22:01:11 +0100

    fix loosing message + optimisation/factorization

--- src/module/feature/loops/enums/shell.enum.ts
+++ src/module/feature/loops/enums/shell.enum.ts
@@ -1,9 +1,35 @@
 /**
- * Enum that transform the wot shell
+ * Enum representing types of ammunition shells.
  */
 export enum ShellEnum {
+    /**
+     * Armor-piercing ammunition.
+     */
     ARMOR_PIERCING = 'AP',
+    /**
+     * Armor-piercing composite rigid ammunition.
+     */
     ARMOR_PIERCING_CR = 'APCR',
+    /**
+     * High-explosive ammunition.
+     */
     HIGH_EXPLOSIVE = 'HE',
+    /**
+     * Hollow charge ammunition.
+     */
     HOLLOW_CHARGE = 'HEAT',
+}
+
+/**
+ * Enum representing types of shell.
+ */
+export enum ShellType {
+    /**
+     * Gold (special) shell type.
+     */
+    GOLD = 'GOLD',
+    /**
+     * Normal shell type.
+     */
+    NORMAL = 'NORMAL',
 }
