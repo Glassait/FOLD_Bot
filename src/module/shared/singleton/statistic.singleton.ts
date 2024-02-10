9da24729fb33e0877cd34a914a01c3b7b2eba249
glassait <tendzinroffler@gmail.com>
2024-02-10 13:51:22 +0100

    Add fold recruitment stats type

--- src/module/shared/singleton/statistic.singleton.ts
+++ src/module/shared/singleton/statistic.singleton.ts
@@ -1,6 +1,12 @@
 import { Logger } from '../classes/logger';
 import { Context } from '../classes/context';
-import { StatisticType, TriviaPlayerStatisticType, TriviaStatisticType } from '../types/statistic.type';
+import {
+    FoldRecruitmentClanStatisticType,
+    FoldRecruitmentStatisticType,
+    StatisticType,
+    TriviaPlayerStatisticType,
+    TriviaStatisticType,
+} from '../types/statistic.type';
 import { FileUtil } from '../utils/file.util';
 import { readFileSync } from 'fs';
 
@@ -15,36 +21,34 @@ export class StatisticSingleton {
 
     /**
      * The path to the statistic file
-     * @private
      */
     private path: string = './src/module/core/statistic.json';
     /**
      * The logger to log thing
-     * @private
      */
     private readonly logger: Logger = new Logger(new Context(StatisticSingleton.name));
     /**
-     * The initial value for the statistic
-     * Update the corresponding version when changing the architecture/type of the data
-     * @private
+     * Represents the initial value for the statistics data.
      */
     private readonly INITIAL_VALUE: StatisticType = {
-        version: 1,
+        version: 2,
         trivia: {
             version: 3,
             overall: {},
             player: {},
         },
+        fold_recruitment: {
+            version: 1,
+            clan: {},
+        },
     };
     /**
      * The statistic
-     * @private
      */
     private readonly _data: StatisticType = this.INITIAL_VALUE;
 
     /**
      * Private constructor to respect singleton pattern
-     * @private
      */
     private constructor() {
         this._data = JSON.parse(readFileSync(this.path).toString());
@@ -55,7 +59,6 @@ export class StatisticSingleton {
 
     /**
      * The instance of the class, used for the singleton pattern
-     * @private
      */
     private static _instance: StatisticSingleton;
 
@@ -70,28 +73,103 @@ export class StatisticSingleton {
         return this._instance;
     }
 
+    //region TRIVIA
     /**
-     * Getter for the trivia game's statistic
+     * Gets the trivia-related statistics, including overall and player-specific data.
+     *
+     * @returns {TriviaStatisticType} - Trivia-related statistics.
+     *
+     * @example
+     * ```typescript
+     * const triviaStats = instance.trivia;
+     * console.log(triviaStats); // { version: 3, overall: {}, player: {} }
+     * ```
      */
     public get trivia(): TriviaStatisticType {
         return this._data.trivia;
     }
 
     /**
-     * Setter for the trivia game's statistic
-     * @param trivia
+     * Sets the trivia-related statistics and writes the updated data to the JSON file.
+     *
+     * @param {TriviaStatisticType} trivia - The updated trivia-related statistics.
+     *
+     * @example
+     * ```typescript
+     * const newTriviaStats = { version: 4, overall: { `updated overall data` }, player: { `updated player data` } };
+     * instance.trivia = newTriviaStats;
+     * ```
      */
     public set trivia(trivia: TriviaStatisticType) {
         this._data.trivia = trivia;
         FileUtil.writeIntoJson(this.path, this._data);
     }
+    //endregion
+
+    //region FOLD RECRUITMENT
+    /**
+     * Gets the fold recruitment-related statistics, including version information and clan-specific data.
+     *
+     * @returns {FoldRecruitmentStatisticType} - Fold recruitment-related statistics.
+     *
+     * @example
+     * ```typescript
+     * const foldRecruitmentStats = instance.foldRecruitment;
+     * console.log(foldRecruitmentStats); // { version: 1, clan: {} }
+     * ```
+     */
+    public get foldRecruitment(): FoldRecruitmentStatisticType {
+        return this._data.fold_recruitment;
+    }
 
     /**
-     * Getter for the trivia game's player statistic's
-     * @param playerId The id of the player
-     * @return The statistic of the player
+     * Sets the fold recruitment-related statistics and writes the updated data to the JSON file.
+     *
+     * @param {FoldRecruitmentStatisticType} foldRecruitment - The updated fold recruitment-related statistics.
+     *
+     * @example
+     * ```typescript
+     * const newFoldRecruitmentStats = { version: 2, clan: { `updated clan data` } };
+     * instance.foldRecruitment = newFoldRecruitmentStats;
+     * ```
+     */
+    public set foldRecruitment(foldRecruitment: FoldRecruitmentStatisticType) {
+        this._data.fold_recruitment = foldRecruitment;
+        FileUtil.writeIntoJson(this.path, this._data);
+    }
+    //endregion
+
+    /**
+     * Gets the player-specific trivia statistics for a specific player.
+     *
+     * @param {string} playerId - The ID of the player for whom to retrieve statistics.
+     * @returns {TriviaPlayerStatisticType} - Player-specific trivia statistics.
+     *
+     * @example
+     * ```typescript
+     * const playerId = '123456789';
+     * const playerStats = instance.getPlayerStatistic(playerId);
+     * console.log(playerStats); // { elo: 1500, participation: 5, right_answer: 2, answer_time: [ `array of answer times` ], win_strick: { current: 1, max: 2 } }
+     * ```
      */
     public getPlayerStatistic(playerId: string): TriviaPlayerStatisticType {
         return this._data.trivia.player[playerId];
+    }
+
+    /**
+     * Gets the clan-specific fold recruitment statistics for a specific clan.
+     *
+     * @param {string} clanID - The ID of the clan for whom to retrieve statistics.
+     * @returns {FoldRecruitmentClanStatisticType} - Clan-specific fold recruitment statistics.
+     *
+     * @example
+     * ```typescript
+     * const clanID = 'ABC123';
+     * const clanStats = instance.getClanStatistic(clanID);
+     * console.log(clanStats); // { leaving_player: 5 }
+     * ```
+     */
+    public getClanStatistic(clanID: string): FoldRecruitmentClanStatisticType {
+        return this._data.fold_recruitment.clan[clanID];
     }
 }
--- src/module/shared/types/statistic.type.ts
+++ src/module/shared/types/statistic.type.ts
@@ -1,54 +1,126 @@
+//region TRIVIA
 /**
- * The monthly overall statistic for the trivia game
+ * Represents the overall statistics for a specific month in the trivia game.
  */
 export type MonthlyTriviaOverallStatisticType = {
+    /**
+     * The total number of games start by the bot, during the month
+     */
     number_of_game: number;
+    /**
+     * The total number of games without participation, during the month
+     */
     game_without_participation: number;
+    /**
+     * The list of unique tanks selected, during the month
+     */
     unique_tanks?: string[];
 };
 
 /**
- * Definition of the architecture of the overall statistic for the trivia game
+ * Represents the overall statistics for the trivia game, categorized by month.
  */
 export type TriviaOverallStatisticType = {
     [key: string]: MonthlyTriviaOverallStatisticType;
 };
 
 /**
- * The monthly statistic of the player for the trivia game
+ * Represents the player-specific statistics for a specific month in the trivia game.
  */
 export type MonthlyTriviaPlayerStatisticType = {
+    /**
+     * The elo of the player during the month
+     */
     elo: number;
+    /**
+     * The participation of the player during the month
+     */
     participation: number;
+    /**
+     * The total number of right answer given by the player during the month
+     */
     right_answer: number;
+    /**
+     * The time taken by the player to answer the trivia question during the month
+     */
     answer_time: number[];
+    /**
+     * The winning streak of the player during the month
+     */
     win_strick: { current: number; max: number };
 };
 
 /**
- * Definition of the architecture of the player statistic for the trivia game
+ * Represents the player-specific statistics for the trivia game, categorized by player.
  */
 export type TriviaPlayerStatisticType = {
     [key: string]: MonthlyTriviaPlayerStatisticType;
 };
 
 /**
- * Definition of all the statistics for the trivia game
- * Update the version when updating the architecture/type
+ * Represents the overall statistics for the trivia game, including both overall and player-specific data.
  */
 export type TriviaStatisticType = {
+    /**
+     * The version number of the trivia statistics data.
+     * @update It each time the architecture changes
+     */
     version: number;
     overall: TriviaOverallStatisticType;
     player: {
         [key: string]: TriviaPlayerStatisticType;
     };
 };
+//endregion
+
+//region FOLD RECRUITMENT
+/**
+ * Represents the monthly statistics for fold recruitment in a specific clan.
+ */
+export type MonthlyFoldRecruitmentClanStatisticType = {
+    leaving_player: number;
+};
 
 /**
- * Definition of the architecture of the statistic.json file.
- * Update the version when updating the architecture/type
+ * Represents the fold recruitment statistics for multiple months, categorized by month.
+ */
+export type FoldRecruitmentClanStatisticType = {
+    [key: string]: MonthlyFoldRecruitmentClanStatisticType;
+};
+
+/**
+ * Represents the overall fold recruitment statistics, including both version information and clan-specific data.
+ */
+export type FoldRecruitmentStatisticType = {
+    /**
+     * The version number of the fold recruitment statistics data.
+     * @update It each time the architecture changes
+     */
+    version: number;
+    /**
+     * Clan-specific fold recruitment statistics, categorized by clan id.
+     */
+    clan: {
+        [key: string]: FoldRecruitmentClanStatisticType;
+    };
+};
+//endregion
+
+/**
+ * Represents the structure of a JSON file containing statistics data.
  */
 export type StatisticType = {
+    /**
+     * The version number of the statistics data.
+     * @update It each time the architecture changes
+     */
     version: number;
+    /**
+     * Trivia-related statistics, including overall and player-specific data.
+     */
     trivia: TriviaStatisticType;
+    /**
+     * Statistics related to fold recruitment.
+     */
+    fold_recruitment: FoldRecruitmentStatisticType;
 };
