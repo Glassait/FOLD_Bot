6309ce59f52931ed267327c25001b4a02a888eba
Tendzin ROFFLER <40204866+Glassait@users.noreply.github.com>
2024-06-22 12:34:31 +0200

    release: Add log to wot wot news forum (#233)

--- src/module/feature/events/models/wot-news-forum.model.ts
+++ src/module/feature/events/models/wot-news-forum.model.ts
@@ -3,25 +3,62 @@ import { ChannelsTable } from '../../../shared/tables/complexe-table/channels/ch
 import { Client, Embed, EmbedBuilder, ForumChannel, Message, Snowflake } from 'discord.js';
 import { fetchChannelFromClient } from '../../../shared/utils/user.util';
 import { isDev } from '../../../shared/utils/env.util';
+import { LoggerInjector } from '../../../shared/decorators/injector/logger-injector.decorator';
+import { Logger } from '../../../shared/utils/logger';
 
+@LoggerInjector
 export class WotNewsForumModel {
+    //region INJECTION
     @Table('Channels') private readonly channelsTable: ChannelsTable;
+    //endregion
 
-    public readonly guildId: string[] = ['1184852469164027976'];
+    //region PUBLIC READONLY FIELDS
+    /**
+     * List if guild id, where the crosspost check
+     */
+    public readonly guildId: Snowflake[] = ['1184852469164027976'];
 
-    public readonly channelId: string[] = ['1185175797628149790'];
+    /**
+     * List if channel id, where the crosspost check
+     */
+    public readonly channelId: Snowflake[] = ['1185175797628149790'];
 
-    public readonly wotFr: Snowflake = '1245366806915579914';
+    /**
+     * List of author id, who are accepted to crosspost
+     */
+    public readonly authorId: Snowflake[];
+    //endregion
 
-    public readonly wot: Snowflake = '1190773147297914900';
+    private readonly logger: Logger;
 
-    public readonly authorId: string[] = [this.wot, this.wotFr];
+    /**
+     * Id of the wot fr user
+     */
+    private readonly wotFr: Snowflake = '1245366806915579914';
 
+    /**
+     * Id of the wot user
+     */
+    private readonly wot: Snowflake = '1190773147297914900';
+
+    /**
+     * The forum channel to post inside
+     */
     private channel: ForumChannel;
 
+    /**
+     * The id of default tag in the forum channel
+     */
     private noTagId: Snowflake;
 
+    /**
+     * The id of the code tag in the forum channel
+     */
+    private codeTagId: Snowflake;
+
     constructor(client: Client) {
+        this.authorId = [this.wot, this.wotFr];
+
         if (!isDev()) {
             return;
         }
@@ -31,17 +68,35 @@ export class WotNewsForumModel {
         this.guildId.push('1218558386761891901');
     }
 
+    /**
+     * Initialize the model, by fetching the channel
+     *
+     * @param {Client} client - The discord client instance
+     */
     public async initialize(client: Client): Promise<void> {
         this.channel = await fetchChannelFromClient(client, await this.channelsTable.getWotNews());
         this.noTagId = this.channel.availableTags.find(({ name }): boolean => name.toLowerCase() === 'Autres nouveautés'.toLowerCase())!.id;
+        this.codeTagId = this.channel.availableTags.find(({ name }): boolean => name.toLowerCase() === 'Code'.toLowerCase())!.id;
     }
 
+    /**
+     * Crosspost message to the forum channel
+     *
+     * @param {Message} message - The message to crosspost
+     */
     public async crosspostMessage(message: Message): Promise<void> {
+        this.logger.debug(
+            'Message to crosspost detected, author {}, name {}',
+            message.author.id,
+            message.author.globalName ?? message.author.displayName
+        );
+
         if (message.author.id === this.wotFr || (isDev() && message.author.id === this.authorId[2])) {
+            this.logger.debug('Crosspost wot fr message (bonus code)');
             await this.channel.threads.create({
                 name: 'Code bonus',
                 message: { content: message.content },
-                appliedTags: [this.noTagId],
+                appliedTags: [this.codeTagId],
             });
 
             return;
@@ -55,6 +110,7 @@ export class WotNewsForumModel {
         if (!tags.length) {
             tags.push(this.noTagId);
         }
+        this.logger.debug('Crosspost wot message with tag [{}]', tags.join(', '));
 
         await this.channel.threads.create({
             name: embed.title!,
