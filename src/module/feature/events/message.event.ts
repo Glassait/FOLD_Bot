ef29762d34ea8c8e6bdc5f4229cd0ef867344f6d
glassait <tendzinroffler@gmail.com>
2024-07-08 00:08:56 +0200

    Refactor wot crosspost message

--- src/module/feature/events/message.event.ts
+++ src/module/feature/events/message.event.ts
@@ -1,19 +1,29 @@
 import { type Client, Events, Message } from 'discord.js';
 import type { BotEvent } from './types/bot-event.type';
 import { WotNewsForumModel } from './models/wot-news-forum.model';
+import { Logger } from '../../shared/utils/logger';
+import { basename } from 'node:path';
+
+const logger: Logger = new Logger(basename(__filename));
 
 module.exports = {
     name: Events.MessageCreate,
-    async execute(client: Client, interaction: Message): Promise<void> {
-        const wotNewsForumModel: WotNewsForumModel = new WotNewsForumModel(client);
+    async execute(client: Client, message: Message): Promise<void> {
+        if (!message.webhookId) {
+            return;
+        }
 
-        if (
-            wotNewsForumModel.guildId.includes(interaction.guildId!) &&
-            wotNewsForumModel.channelId.includes(interaction.channelId) &&
-            wotNewsForumModel.authorId.includes(interaction.author.id)
-        ) {
+        logger.debug(
+            'Webhook message detected, id {}, author name {}',
+            message.webhookId,
+            message.author.displayName ?? message.author.globalName
+        );
+        const wotNewsForumModel: WotNewsForumModel = new WotNewsForumModel();
+
+        if (wotNewsForumModel.canBeCrosspost(message)) {
+            logger.debug('Webhook message detected can be crosspost');
             await wotNewsForumModel.initialize(client);
-            await wotNewsForumModel.crosspostMessage(interaction);
+            await wotNewsForumModel.crosspostMessage(message);
         }
     },
 } as BotEvent;
